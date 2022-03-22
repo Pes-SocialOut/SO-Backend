78bc34c23b80f39af2705c47252fc1a7f2a4bcb6
Franco Acevedo <franco.acevedo@estudiantat.upc.edu>
2022-03-22 17:41:42 +0100

    task #59: Api connection

--- api/app/module_airservice/data_airService.py
+++ api/app/module_airservice/data_airService.py
@@ -2,6 +2,8 @@ from urllib import response
 import requests
 import json
 
+#from api.app.module_domain.models import AirQualityData, AirQualityStation
+
 
 #llamadas a la API que contiene datos sobre la contaminación del aire
 url = 'https://analisi.transparenciacatalunya.cat/resource/tasf-thgu.json?data=2022-03-20&nom_estacio=Gavà&$order=codi_eoi'
@@ -22,12 +24,14 @@ for d in response_json:
 codi_eoi=0; nom_estacio=''; contaminant=''; unitats=''; tipus_estacio=''; area_urbana='';magnitud = 0
 h1=0; h2=0; h3=0; h4=0; h5=0; h6=0; h7=0; h8=0; h9=0; h10=0; h11=0; h12=0
 h13=0; h14=0; h15=0; h16=0; h17=0; h18=0; h19=0; h20=0; h21=0; h22=0; h23=0; h24=0
-altitud=''; latitud=''; longitud=''; resultado_final=0.0; 
+altitud=''; geocoded=''
+
+
 
 primero = 1
 codi_previ = ''
 horas = {}
-contaminantes={}
+
 
 for d in response_json:
 
@@ -38,12 +42,9 @@ for d in response_json:
         codi_eoi = codi_actual
 
 
-    
     for key in d:
         if codi_actual != codi_previ:
-            #-----------> Calculo de heuristica y envio a BD
             codi_eoi = codi_actual
-            contaminantes.clear()
 
         else:
             if key == 'nom_estacio': nom_estacio = d[key]
@@ -53,40 +54,27 @@ for d in response_json:
             elif key == 'magnitud': magnitud = d[key]
             elif key == 'contaminant': contaminant = d[key]
             elif key == 'altitud': altitud = d[key]
-            elif key == 'latitud': latitud = d[key]
-            elif key == 'longitud': longitud = d[key]
+            elif key == 'geocoded_column': geocoded = d[key]
             else:
-                if key!='data' and key!='codi_ine' and key!='municipi' and key!='codi_comarca' and key!='nom_comarca' and key!='geocoded_column' and key!= 'codi_eoi': 
+                if key!='data' and key!='codi_ine' and key!='municipi' and key!='codi_comarca' and key!='nom_comarca' and key!='geocoded_column' and key!= 'codi_eoi' and key!='latitud' and key!='longitud': 
                     horas[key] = d[key]
     
-    
-    for elem in horas:
-        resultado_final = resultado_final + float(horas[elem])
+    #mandar a tabla AirQualityStation
 
-    if len(horas) > 0:
-        resultado_final = resultado_final/len(horas)
+    #x = AirQualityStation(nom_estacio,codi_actual,tipus_estacio,area_urbana,int(altitud),geocoded)
+    #x.save()
 
-    #print(d['contaminant'])
-    contaminantes[d['contaminant']] = resultado_final 
-    #print('resultado_final: ', resultado_final)
+    #for elem in horas:
+        #y = AirQualityData()
 
     h1=0; h2=0; h3=0; h4=0; h5=0; h6=0; h7=0; h8=0; h9=0; h10=0; h11=0; h12=0
     h13=0; h14=0; h15=0; h16=0; h17=0; h18=0; h19=0; h20=0; h21=0; h22=0; h23=0; h24=0
-    resultado_final = 0.0
     horas.clear()
 
 
     codi_previ = codi_actual
 
 
-
-
-
 #AirQualityStation.__init__()
 
-
-#calculo de heuristica para determinar la calidad final del aire
-def heuristica_contaminantes():
-    
-    print()
 
