46e12b25adaa2d3718e0b3c28c78ed491f3f059c
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-03-24 09:16:03 +0100

    Fix api docker to communicate properly with db docker

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -24,7 +24,7 @@ logger = logging.getLogger('alembic.env')
 from flask import current_app
 config.set_main_option(
     'sqlalchemy.url', current_app.config.get(
-        'SQLALCHEMY_DATABASE_URI').replace('%', '%%'))
+        'MIGRATIONS_SQLALCHEMY_DATABASE_URI').replace('%', '%%'))
 target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
