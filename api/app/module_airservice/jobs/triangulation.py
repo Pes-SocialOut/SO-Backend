2333d79444f81f38412782f93e07299741a78949
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-03-24 19:53:17 +0100

    task #67: Implement main functionality

--- api/app/module_airservice/jobs/triangulation.py
+++ api/app/module_airservice/jobs/triangulation.py
@@ -1,31 +1,66 @@
-# Import the database object from the main app module
-from app import db
-
-# Import module models
-from app.module_airservice.models import AirQualityData, AirQualityStation
+# Import db connection library
+import os
+from sqlalchemy import create_engine
 
 # Import triangulations library
 import matplotlib.tri as mtri
-import numpy as np
 
-def fetch_air_data() -> None:
-    print('not implemented')
-    print(AirQualityData.query.filter_by(DateHour='2022-03-21').order_by(AirQualityData.EOIcodeStation.desc()).all())
-    # Get all AirQualityStations from measurments 24h ago
-    # for each AirQualityStation:
-        # Get all measurements of airstation+date(24h ago).
-        # Calculate a ponderated weight "quality" from the measurements
-        # Add airstation + quality to datastructure
+# Import time libraries
+from datetime import datetime, timedelta
 
-def triangulate() -> mtri.Triangulation:
-    print("not implemented")
+# Import pickle library to save python objects to file
+import pickle as pkl
+
+triangulation_file_path = 'triangulation.pkl'
+engine = create_engine(os.getenv("MIGRATIONS_SQLALCHEMY_DATABASE_URI"))
+
+def fetch_air_data() -> list:
+    yesterday = datetime.now() - timedelta(days = 1)
+    if yesterday.minute > 30:
+        yesterday += timedelta(hours = 1)
+    qyear = yesterday.year
+    qmonth = yesterday.month
+    qday = yesterday.day
+    qhour = yesterday.hour
+    qstring = f'{qyear}-{qmonth}-{qday} {qhour}:00:00'
+
+    with engine.connect() as conn:
+        # 1st approach: take average -> next apply ponderation following heuristic
+        air_data = conn.execute(
+            'SELECT aqs.eoi_code, aqs.lon, aqs.lat, avg(aqd.quality) \
+            FROM air_quality_data aqd inner join air_quality_station aqs on aqd.eoi_code = aqs.eoi_code \
+            WHERE aqd.date_hour = %s \
+            GROUP BY aqd.eoi_code, aqs.lon, aqs.lat;',
+            (qstring)
+        ).fetchall()
+    
+    print(air_data)
+    return air_data
+
+def update_station_general_quality(air_data) -> None:
+    for entry in air_data:
+        eoi_code = entry[0]
+        general_quality = entry[3]
+        with engine.connect() as conn:
+            conn.execute(
+                'UPDATE air_quality_station \
+                SET last_quality = %s \
+                WHERE eoi_code = %s;',
+                (general_quality, eoi_code)
+            )
+
+def triangulate(air_data) -> mtri.Triangulation:
     # create numpy arrays for triangulation
-    # return mtri.triangulate(x,y)
+    # Testing triangulation
+    #x = [1,4,2,0,3,2,5,1,3]
+    #y = [4,4,3,2,2,1,1,0,0]
+    x = list(map(lambda x: x[1], air_data))
+    y = list(map(lambda x: x[2], air_data))
+    return mtri.Triangulation(x,y)
 
-def save_current_triangulation() -> None:
-    print("not implemented")
-    # save pkl with datastructure and Triangulation
-        #(to be read by endpoint air/point when interpolating the aprox. value)
+def save_current_triangulation(triangulation, air_data) -> None:
+    with open(triangulation_file_path, 'w') as out:
+        pkl.dump({'tri':triangulation, 'idx_data': air_data}, out, pkl.HIGHEST_PROTOCOL)
 
 def generate_heat_map() -> None:
     print("not implemented")
@@ -34,5 +69,23 @@ def generate_heat_map() -> None:
     #  as color values of vertices
 
 if __name__ == '__main__':
+    #air_data = fetch_air_data()
+    #update_station_general_quality(air_data)
+    #triangulation = triangulate(air_data)
+    #save_current_triangulation(triangulation, air_data)
+
     print('main')
     # run script every hour
+    triangulation = triangulate([(-1,0,0,-1),(-1,1,1,-1),(-1,0,2,-1),(-1,1,-1,-1)])
+    trifinder = triangulation.get_trifinder()
+    print(triangulation.triangles)
+    print(trifinder([.5],[0.4]))
+    exit()
+    dt = datetime(year=2022, month=1, day=1, hour = 3) - timedelta(days=1)
+    print(dt.year)
+    print(dt.month)
+    print(dt.day)
+    print(dt.hour)
+    print(dt.minute)
+
+    print(DateTime().python_type(year=2022, month=3, day=24))
