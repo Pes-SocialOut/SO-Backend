f8765e7cdee1558379961338695826e70df0104e
LearningBizz <learningbizz@gmail.com>
2022-03-25 12:11:06 +0100

    POST event in progress

--- api/app/module_event/controllers.py
+++ api/app/module_event/controllers.py
@@ -1,10 +1,10 @@
 # Import flask dependencies
 # Import module models (i.e. User)
 from app.module_event.models import Event
-from datetime import datetime
+from datetime import date, datetime
 from sqlalchemy.dialects.postgresql import UUID
 from flask import (Blueprint, flash, g, redirect, render_template, request,
-                   session, url_for)
+                   session, url_for, jsonify)
 import uuid
 
 # Import the database object from the main app module
@@ -18,7 +18,45 @@ module_event = Blueprint('event', __name__, url_prefix='/events')
 # POST method: creates an event in the database
 @module_event.route('', methods=['POST'])
 def create_event():
-    return "Always successful POST", 200
+    args = request.args
+
+    # restriction 1: check for vulgar words in description and name
+
+    # restriction 2: String attributes aren't empty    
+    if len(args.get("id")) == 0 | len(args.get("name")) == 0 | len(args.get("description")) == 0 | len(args.get("userCreator")) == 0:
+        return "An attribute is empty!", 400
+    
+    # restriction 3: date started is older than end date of the event
+    dateStarted = datetime.strptime(args.get("dateStarted"), '%Y-%m-%d %H:%M:%S.%f')
+    dateEnd= datetime.strptime(args.get("dateEnd"), '%Y-%m-%d %H:%M:%S.%f')
+    #'2015-06-05 10:20:10.000'
+    
+    if dateStarted > dateEnd:
+        return "date Started is bigger than date End, that's not possible!", 400
+    
+    # restriction 4: longitud and latitude in Catalunya
+
+    # restriction 5: date started should be right now or in the future
+
+    #STUCK HERE WITH THE DATES! I NEED TO GET THE DATESTARTED AND DATEEND AND DO COMPARISONS WITH THEM IN DATETIME FORMAT
+    now = datetime.today
+    if dateStarted < now:
+        return "date Started earlier than right now, it already started?", 400
+
+    # restriction 6: description attribute is longer than 250 characters
+    if len(args.get("description")) > 250:
+        return "Description is too long!", 400
+    
+    # restriction 7: name attribute is longer than 25 characters
+    if len(args.get("name")) > 25:
+        return "Name is too long!", 400
+    
+    
+    event = Event(uuid.UUID(args.get("id")), args.get("name"), args.get("description"), args.get("dateCreated"), args.get("dateEnd"), uuid.UUID(args.get("userCreator")), args.get("longitud"), args.get("latitude"))
+    db.session.add(event)
+    db.session.commit()
+    
+    return jsonify({'event': event}), 201
 
 # GET method: returns the information of one event
 @module_event.route('', methods=['GET'])
@@ -39,7 +77,6 @@ def get_all_events():
 @module_event.route('', methods=['PUT'])
 def modify_events():
     return "Always successful PUT", 200
-
 
 # If the event doesn't exist
 @module_event.errorhandler(404)
--- api/app/module_event/models.py
+++ api/app/module_event/models.py
@@ -13,19 +13,19 @@ class Event(db.Model):
     # Event id
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     # Event name
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     # Event description
-    description = db.Column(db.String)
+    description = db.Column(db.String, nullable=False)
     # Start date of the event
-    dateStarted = db.Column(db.DateTime)
+    dateStarted = db.Column(db.DateTime, nullable=False)
     # End date of the event
-    dateEnd = db.Column(db.DateTime)
+    dateEnd = db.Column(db.DateTime, nullable=False)
     # Creator of the event
-    userCreator = db.Column(UUID(as_uuid=True))
+    userCreator = db.Column(UUID(as_uuid=True), nullable=False)
     # Longitude of the location where the event will take taking place
-    longitud = db.Column(db.Float)
+    longitud = db.Column(db.Float, nullable=False)
     # Latitude of the location where the event will take taking place
-    latitude = db.Column(db.Float)
+    latitude = db.Column(db.Float, nullable=False)
 
     # New instance instantiation procedure
     def __init__(self, id, name, description, dateCreated, dateEnd, userCreator, longitud, latitude):
