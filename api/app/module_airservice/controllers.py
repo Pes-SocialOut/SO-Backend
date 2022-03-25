05b1aaa9394f9ff97c5c11aa0e2f5d30eb9d2a4d
tipeXwins <victorymuchi2013@gmail.com>
2022-03-25 19:49:07 +0100

    task #66: models air service updated

--- api/app/module_airservice/controllers.py
+++ api/app/module_airservice/controllers.py
@@ -5,7 +5,7 @@ from flask import Blueprint, request, render_template, flash, g, session, redire
 from app import db
 
 # Import module models
-from app.module_airservice.models import DateHour, AirQualityStation, AirQualityData
+from app.module_airservice.models import date_hour, air_quality_station, air_quality_data
 
 # Define the blueprint: 'air', set its url prefix: app.url/air
 module_airservice = Blueprint('air', __name__, url_prefix='/air')
@@ -14,7 +14,7 @@ module_airservice = Blueprint('air', __name__, url_prefix='/air')
 @module_airservice.route('/station/<id>', methods=['GET'])
 def air_station_readings(id):
     #result = AirQualityData.query.filter_by(EOIcodeStation=id).all()
-    dh = DateHour(db.func.current_timestamp())
+    dh = date_hour(db.func.current_timestamp())
     db.session.add(dh)
     db.session.commit()
     return "Success", 200
--- api/app/module_airservice/models.py
+++ api/app/module_airservice/models.py
@@ -1,13 +1,12 @@
 from app import db
 
-class DateHour(db.Model):
-    __tablename__ = 'DateHour'
-    DateHour =db.Column(db.DateTime, default=db.func.current_timestamp() , primary_key=True)
+class date_hour(db.Model):
+    __tablename__ = 'date_hour'
+    date_hour =db.Column(db.DateTime, default=db.func.current_timestamp() , primary_key=True)
     
 
-    def __init__(self, DateHour):
-        """initialize with name."""
-        self.DateHour = DateHour
+    def __init__(self, date_hour):
+        self.date_hour = date_hour
 
     def save(self):
         db.session.add(self)
@@ -15,79 +14,53 @@ class DateHour(db.Model):
 
     @staticmethod
     def get_all():
-        return DateHour.query.all()
+        return date_hour.query.all()
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
     def __repr__(self):
-        return "".format(self.DateHour)
+        return "".format(self.date_hour)
 
 import enum 
-class StationType(enum.Enum):
-    TRAFFIC = "Traffic"
-    BACKGROUND = "Background"
-    INDUSTRIAL = "Industrial"
+class station_type(enum.Enum):
+    traffic = "Traffic"
+    background = "Background"
+    industrial = "Industrial"
 
-class UrbanArea(enum.Enum):
-    URBAN = "Urban"
-    PERIURBAN = "Peri-Urban"
-    RURAL = "Rural"
+class urban_area(enum.Enum):
+    urban = "Urban"
+    periurban = "Peri-Urban"
+    rural = "Rural"
 
 
-class AirQualityStation(db.Model):
-    __tablename__ = 'AirQualityStation'
+class air_quality_station(db.Model):
+    __tablename__ = 'air_quality_station'
 
-    Name = db.Column(db.String)
-    EOIcode = db.Column(db.String, primary_key=True)
-    StationType = db.Column(db.Enum(StationType))
-    UrbanArea = db.Column(db.Enum(UrbanArea))
-    Height = db.Column(db.Integer)
-    GeoReference = db.Column(db.String)
+    name = db.Column(db.String)
+    eoi_code = db.Column(db.String, primary_key=True)
+    station_type = db.Column(db.Enum(station_type))
+    urbanArea = db.Column(db.Enum(urban_area))
+    height = db.Column(db.Integer)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    air_condition_scale = db.Column(db.Float)
+    time_computation_scale = db.Column(db.DateTime)
     #Ubicacion = db.Column(db.Integer, db.ForeignKey("Identificador")) 
 
-    def __init__(self, Name, Eoicode, StationType, UrbanArea, Height, GeoReference):
-        """initialize with name."""
-        self.Name = Name
-        self.EOIcode = Eoicode
-        self.StationType = StationType
-        self.UrbanArea = UrbanArea
-        self.Height = Height
-        self.GeoReference = GeoReference
+    def __init__(self, name, eoi_code, station_type, urban_area, height, latitude, lenght, air_condition_scale, time_computation_scale):
+        
+        self.name = name
+        self.eoi_code = eoi_code
+        self.station_type = station_type
+        self.urban_area = urban_area
+        self.height = height
+        self.latitude = latitude
+        self.lenght = lenght
+        self.air_condition_scale = air_condition_scale
+        self.time_computation_scale = time_computation_scale
 
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @staticmethod
-    def get_all():
-        return AirQualityStation.query.all()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def __repr__(self):
-        return "".format(self.Name, self.EOIcode, self.StationType, self.UrbanArea, self.Height, self.GeoReference)
-
-class AirQualityData(db.Model):
-    __tablename__ = 'AirQualityData'
-
-    DateHour = db.Column(db.DateTime, db.ForeignKey(DateHour.DateHour), primary_key=True)
-    EOIcodeStation = db.Column(db.String, db.ForeignKey(AirQualityStation.EOIcode), primary_key=True)
-    
-    Magnitude = db.Column(db.Integer)
-    PollutantComposition = db.Column(db.String)
-    Units = db.Column(db.String)
-
-    def __init__(self, DateHour, EOIcodeStation, Magnitude, PollutantComposition, Units):
-        """initialize with name."""
-        self.DateHour = DateHour
-        self.EOIcodeStation = EOIcodeStation
-        self.Magnitude = Magnitude
-        self.PollutantComposition = PollutantComposition
-        self.Units = Units
 
 
     def save(self):
@@ -96,11 +69,81 @@ class AirQualityData(db.Model):
 
     @staticmethod
     def get_all():
-        return AirQualityData.query.all()
+        return air_quality_station.query.all()
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
     def __repr__(self):
-        return "".format(self.DateHour, self.EOIcode, self.Magnitude, self.PollutantComposition, self.Units)
+        return "".format(self.name, self.eoi_code, self.station_type, self.urban_area, self.height, self.latitude, self.lenght, self.air_condition_scale, self.time_computation_scale)
+
+class polutant(db.Model):
+    __tablename__ = 'polutant'
+    composition = db.Column(db.String, primary_key=True)
+    common_lowerbound = db.Column(db.Float)
+    common_upperbound = db.Column(db.Float)
+    units = db.Column(db.String)
+
+    def __init__(self, composition, common_lowerbound, common_upperbound, units):
+        
+        self.composition = composition
+        self.common_lowerbound = common_lowerbound
+        self.common_upperbound = common_upperbound
+        self.units = units
+
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return air_quality_station.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "".format(self.composition, self.common_lowerbound, self.common_upperbound, self.units)
+
+
+
+class air_quality_data(db.Model):
+    __tablename__ = 'air_quality_data'
+
+    date_hour = db.Column(db.DateTime, db.ForeignKey(date_hour.date_hour), primary_key=True)
+    station_eoi_code = db.Column(db.String, db.ForeignKey(air_quality_station.eoi_code), primary_key=True)
+    pollutant_composition = db.Column(db.String, db.ForeignKey(polutant.composition), primary_key=True)
+    value = db.Column(db.Float)
+    contaminant_scale = db.Column(db.Float)
+   
+    def __init__(self, date_hour, station_eoi_code, pollutant_composition, value, contaminant_scale):
+       
+        self.date_hour = date_hour
+        self.station_eoi_code = station_eoi_code
+        self.pollutant_composition = pollutant_composition
+        self.value = value
+        self.contaminant_scale = contaminant_scale
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return air_quality_data.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "".format(self.date_hour, self.station_eoi_code, self.pollutant_composition, self.value, self.contaminant_scale)
+
+
+
+
