72646a8be1eb70e06c9599a08916750b3b2fdb9b
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-03-27 21:12:42 +0200

    task #61: Add reasing previous data to script

--- api/app/module_airservice/jobs/data_extraction.py
+++ api/app/module_airservice/jobs/data_extraction.py
@@ -13,25 +13,21 @@ from app.module_airservice.models import station_type, urban_area
 #FIXME: Change MIGRATIONS_ to URI in production. Now it is needed for testing from console.
 engine = create_engine(os.getenv("MIGRATIONS_SQLALCHEMY_DATABASE_URI"))
 
-
 hours = ['h01','h02','h03','h04','h05','h06',
          'h07','h08','h09','h10','h11','h12',
          'h13','h14','h15','h16','h17','h18',
          'h19','h20','h21','h22','h23','h24']
 
-
 date_today = datetime.today()
 date_anterior = date_today - timedelta(days = 1)
 date_anterior = date_anterior.strftime('%Y-%m-%d')
-
-
 
 def insert_air_station(eoi_code, name, station_t, urban_a, altitude, latitude, longitude) -> None:
     try:
         with engine.connect() as conn:
             conn.execute(
                 'INSERT INTO air_quality_station VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)',
-                (eoi_code, name, station_t, urban_a, altitude, latitude, longitude, 0.0, data_today)
+                (name, eoi_code, station_t, urban_a, altitude, latitude, longitude, 0.0, date_today)
             )
     except Exception as err:
         print("Falla inserir estacion")
@@ -39,14 +35,13 @@ def insert_air_station(eoi_code, name, station_t, urban_a, altitude, latitude, l
 
 
 def normalizar(cont, valor) -> float:
-        return round(valor/contaminantes[cont][0],2)
+        return valor/contaminantes[cont][0]
 
 
-def insert_hour_data(hour, codi_eoi, value, pollutant) -> None:
-        hour_2 = hour[-2:] #obtener los dos ultimos caracteres
-        date_hora_final = date_anterior+' '+hour_2+':00:00'
-        if hour_2 == 24: date_hora_final = date_today.strftime('%Y-%m-%d')+' 00:00:00:000000'
-        #date_time_obj = datetime.strptime(date_hora_final, '%Y-%m-%d %H:%M:%S')
+def insert_hour_data(hour_tag, codi_eoi, pollutant, value) -> None:
+        hour = hour_tag[-2:] #obtener los dos ultimos caracteres
+        date_hora_final = date_anterior+' '+hour+':00:00'
+        if hour == 24: date_hora_final = date_today.strftime('%Y-%m-%d')+' 00:00:00:000000'
         cont_scale = normalizar(pollutant,value)
         try:
             with engine.connect() as conn:
@@ -55,26 +50,22 @@ def insert_hour_data(hour, codi_eoi, value, pollutant) -> None:
                    (date_hora_final, codi_eoi, pollutant, value, cont_scale)
                 )
         except Exception as err:
-                with engine.connect() as conn:
-                    res = conn.execute(
-                        'SELECT * FROM air_quality_data WHERE date_hour = %s and station_eoi_code = %s and pollutant_composition = %s;',
-                        (date_hora_final, codi_eoi, pollutant)
-                    ).fetchall()
-                print(res)
-                print("Falla inserir medida")
-                print(err)
-                exit()
+            print("Falla inserir medida")
+            print(err)
+            exit()
             
 
 if __name__ == '__main__':
+
+    # Borrar datos de hace más de un dia
+    with engine.connect() as conn:
+        conn.execute('DELETE FROM air_quality_data WHERE date_hour <= %s;', (date_anterior+' 00:00:00'))
     
-    url = f"https://analisi.transparenciacatalunya.cat/resource/tasf-thgu.json?data={date_anterior}&nom_estacio=Gavà&$order=codi_eoi"
+    url = f"https://analisi.transparenciacatalunya.cat/resource/tasf-thgu.json?data={date_anterior}&$order=codi_eoi"
 
     #llamada a la API que contiene datos sobre la contaminación del aire del dia
     response = requests.get(url)
     response_json = response.json()
-
-    contaminantes_json = [ j['contaminant'] for j in response_json ]
 
     with engine.connect() as conn:
         all_eoi_codes = conn.execute('SELECT DISTINCT eoi_code FROM air_quality_station').fetchall()
@@ -97,17 +88,15 @@ if __name__ == '__main__':
         # Insertar medición
         for hour in hours:
             #no todas las horas existen
-            if medicion[hour] in medicion:
+            if hour in medicion:
                 #insertar hora
                 insert_hour_data(
                     hour,
                     medicion['codi_eoi'],
                     medicion['contaminant'],
-                    medicion[hour]
+                    float(medicion[hour])
                     )
- 
-    print(contaminantes_json)
-    exit()
+
 
 
 
