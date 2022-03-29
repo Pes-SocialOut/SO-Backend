d307bff3d233857f10f06ae23c3083801f0915d2
Guillem García <guillem.garcia.gil@protonmail.com>
2022-03-29 23:04:35 +0200

    Last one maybe

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
 from app import app
 import os
-app.run(host='0.0.0.0', port=int(os.getenv('API_PORT')), debug=bool(os.getenv('API_DEBUG')))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.getenv('API_PORT')), debug=bool(os.getenv('API_DEBUG')))
