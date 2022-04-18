cea9b57be9c0597a8ab3ff600145169963848f00
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-04-18 16:58:54 +0200

    task #105: Create endpoint register/socialout

--- app/module_users/controllers.py
+++ app/module_users/controllers.py
@@ -1,6 +1,7 @@
 # Import flask dependencies
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
+import uuid
 
 # Import the database object from the main app module
 from app import db
@@ -11,7 +12,7 @@ import string
 import random
 
 # Import module models
-from app.module_users.models import User, SocialOutAuth, GoogleAuth, FacebookAuth
+from app.module_users.models import User, SocialOutAuth, GoogleAuth, FacebookAuth, EmailVerificationPendant
 
 # Define the blueprint: 'users', set its url prefix: app.url/users
 module_users_v1 = Blueprint('users', __name__, url_prefix='/v1/users')
@@ -19,10 +20,15 @@ module_users_v1 = Blueprint('users', __name__, url_prefix='/v1/users')
 # Endpoints related to profile
 
 @module_users_v1.route('/<id>', methods=['GET'])
+@jwt_required(optional=True)
 def get_profile(id):
-    # hardcoded auth variable
-    is_authenticated_id = True
-    query_result = User.query.filter_by(id = id).first()
+    auth_id = get_jwt_identity()
+    is_authenticated_id = id == auth_id
+    try:
+        user_id = uuid.UUID(id)
+    except:
+        return jsonify({"error_message": "ID isn't a valid UUID"}), 400
+    query_result = User.query.filter_by(id = user_id).first()
     if query_result == None:
             return jsonify({"error_message":f"User with id {id} does not exist"}), 404
     profile = query_result.toJSON()
@@ -38,13 +44,73 @@ def update_profile(id):
 
 # Endpoints related authentication
 
+@module_users_v1.route("/register/socialout", methods=["POST"])
+def register_socialout():
+    if "email" not in request.json:
+        return jsonify({"error_message": "Email attribute missing in json"}), 400 
+    if "password" not in request.json:
+        return jsonify({"error_message": "Password attribute missing in json"}), 400 
+    if "username" not in request.json:
+        return jsonify({"error_message": "Username attribute missing in json"}), 400 
+    if "description" not in request.json:
+        return jsonify({"error_message": "Description attribute missing in json"}), 400 
+    if "languages" not in request.json:
+        return jsonify({"error_message": "Languages list attribute missing in json"}), 400 
+    if "hobbies" not in request.json:
+        return jsonify({"error_message": "Hobbies list attribute missing in json"}), 400 
+    if "verification" not in request.json:
+        return jsonify({"error_message": "Verification code attribute missing in json"}), 400 
+    
+    email = request.json["email"]
+    pw = request.json["password"]
+    username = request.json["username"]
+    description = request.json["description"]
+    languages = request.json["languages"]
+    hobbies = request.json["hobbies"]
+    verification = request.json["verification"]
+
+    # Check no other user exists with that email
+    email_verification = User.query.filter_by(email = email).first()
+    if email_verification != None:
+        return jsonify({"error_message": "User with this email already exists"}), 400
+    
+    # Check verification code in codes sent to email
+    db_verification = EmailVerificationPendant.query.filter_by(email = email).first()
+    if db_verification == None:
+        return jsonify({"error_message": "Verification code was never sent to this email."}), 400
+    if db_verification.code != verification:
+        return jsonify({"error_message": "Verification code does not coincide with code sent to email"}), 400
+
+    # Add user to bd
+    user_id = uuid.uuid4()
+    user = User(user_id, username, email, None, None, description, hobbies)
+    try:
+        user.save()
+    except:
+        return jsonify({"error_message": "Something went wrong when creating new user in db"}), 500
+    
+    # Add languages to user (once implemented)
+    
+    # Add socialout auth method to user
+    user_salt = get_random_salt(15)
+    hashed_pw = hashing.hash_value(pw, salt=user_salt)
+    socialout_auth = SocialOutAuth(user_id, user_salt, hashed_pw)
+    try:
+        socialout_auth.save()
+    except:
+        return jsonify({"error_message": "Something went wrong when adding auth method socialout to user"}), 500
+    
+    return generate_tokens(str(user_id))
+
+
+
 @module_users_v1.route("/login/socialout", methods=["POST"])
 def login_socialout():
     if not ('email' in  request.json or 'password' in request.json):
         return jsonify({"error_message": "Missing credentials in json body."}), 400 
-    email = request.json.email
-    password = request.json.password
-    user = SocialOutAuth.query.filter_by(email = email).first()
+    email = request.json["email"]
+    password = request.json["password"]
+    user = User.query.filter_by(email = email).first()
     if user == None:
         return jsonify({"error_message": "Email or password are wrong."}), 400 
     socialout_auth = SocialOutAuth.query.filter_by(id = user.id).first()
@@ -52,7 +118,7 @@ def login_socialout():
         return jsonify({"error_message": "Authentication method not available for this email"}), 400 
     if not hashing.check_value(socialout_auth.pw, password, salt=socialout_auth.salt):
         return jsonify({"error_message": "Email or password are wrong."}), 400 
-    return generate_tokens()
+    return generate_tokens(str(user.id))
 
 @module_users_v1.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)
--- app/module_users/models.py
+++ app/module_users/models.py
@@ -17,19 +17,22 @@ class User(db.Model):
     # URL of the reduced-resolution profile image
     mini_profile_img_uri = db.Column(db.String)
     # User description / information
-    description = db.Column(db.String, default="")
+    description = db.Column(db.String, default="", nullable=False)
+    # User hobbies
+    hobbies = db.Column(db.String, default="", nullable=False)
 
     # To CREATE an instance of a User
-    def __init__(self, id, username, email, profile_img_uri, mini_profile_img_uri, description):
+    def __init__(self, id, username, email, profile_img_uri, mini_profile_img_uri, description, hobbies):
         self.id = id
         self.username = username
         self.email = email
         self.profile_img_uri = profile_img_uri
         self.mini_profile_img_uri = mini_profile_img_uri
         self.description = description
+        self.hobbies = hobbies
 
     def __repr__(self):
-        return f'User({self.id}, {self.username}, {self.profile_img_uri}, {self.mini_profile_img_uri}, {self.description})'
+        return f'User({self.id}, {self.username}, {self.profile_img_uri}, {self.mini_profile_img_uri}, {self.description}, {self.hobbies})'
 
     # To DELETE a row from the table
     def delete(self):
@@ -46,9 +49,10 @@ class User(db.Model):
             'id': self.id,
             'username': self.username,
             'email': self.email,
-            'profile_img_uri': self.profile_img_uri.value,
+            'profile_img_uri': self.profile_img_uri,
             'mini_profile_img_uri': self.mini_profile_img_uri,
-            'description': self.description
+            'description': self.description,
+            'hobbies': self.hobbies
         }
 
 class SocialOutAuth(db.Model):
@@ -69,6 +73,29 @@ class SocialOutAuth(db.Model):
 
     def __repr__(self):
         return f'User({self.id}, {self.salt}, {self.pw})'
+
+    # To DELETE a row from the table
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    # To SAVE a row from the table
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+class EmailVerificationPendant(db.Model):
+    __tablename__ = 'email_verification'
+
+    email = db.Column(db.String, primary_key=True, nullable=False)
+    code = db.Column(db.String, nullable=False)
+
+    def __init__(self, email, code):
+        self.email = email
+        self.code = code
+
+    def __repr__(self):
+        return f'User({self.email}, {self.code})'
 
     # To DELETE a row from the table
     def delete(self):
