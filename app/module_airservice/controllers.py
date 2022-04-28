681b548106e249b2736c393b0ee1d6f824d8e1ac
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-04-28 17:28:04 +0200

    add endpoint to process list of points

--- app/module_airservice/controllers.py
+++ app/module_airservice/controllers.py
@@ -107,3 +107,42 @@ def barycentric_interpolation(x1, y1, x2, y2, x3, y3, xp, yp):
     w0 = ((y2-y3)*(xp-x3)+(x3-x2)*(yp-y3))/((y2-y3)*(x1-x3)+(x3-x2)*(y1-y3))
     w1 = ((y3-y1)*(xp-x3)+(x1-x3)*(yp-y3))/((y2-y3)*(x1-x3)+(x3-x2)*(y1-y3))
     return w0, w1, 1-w0-w1
+
+@module_airservice_v1.route('/location/', methods=['POST'])
+def general_quality_at_multiple_points():
+    if 'points' not in request.json:
+        return jsonify({'error_message':'Must contain json with "points" list, each point {ref_id, long, lat}'}), 400
+    point_data = request.json['points']
+
+    try:
+        tri_query_result = triangulation_cache.query.first()
+        triangulation_data = pkl.loads(tri_query_result.tri_object_bytes)
+        triangulation_upd_time = tri_query_result.date_hour
+    except:
+        return jsonify({"error_message":"Service depends on data not currently available, try again later..."}), 404
+    trifinder = triangulation_data['tri'].get_trifinder()
+    triangles = triangulation_data['tri'].triangles
+    air_data = triangulation_data['air']
+
+    response = []
+    for point in point_data:
+        if not ('ref_id' in point and 'long' in point and 'lat' in point):
+            continue
+        ref_id = point['ref_id']
+        long = point['long']
+        lat = point['lat']
+
+        triangle_id = trifinder(long, lat)
+        if triangle_id == -1:
+            continue
+
+        triangle = triangles[triangle_id]
+        
+        s0 = air_data[triangle[0]]
+        s1 = air_data[triangle[1]]
+        s2 = air_data[triangle[2]]
+        w0, w1, w2 = barycentric_interpolation(s0[1], s0[2], s1[1], s1[2], s2[1], s2[2], long, lat)
+        general_quality = w0*s0[3] + w1*s1[3] + w2*s2[3]
+
+        response.append({'ref_id': ref_id, 'pollution': general_quality})
+    return jsonify({'triangulation_last_calculated_at': triangulation_upd_time, 'points': response}), 200
