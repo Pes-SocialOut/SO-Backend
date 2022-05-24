0fde595a9c214272c68506d6319945b82575cd8c
tipeXwins <victorymuchi2013@gmail.com>
2022-05-24 17:55:51 +0200

    Google calendar Api crear modificar borrar todo funcionando

---
+++ app/module_calendar/functions_calendar.py
@@ -0,0 +1,155 @@
+from quickstart import get_calendar_service
+from geopy.geocoders import Nominatim
+
+
+def main():
+    service = get_calendar_service()
+
+    event = {
+    'summary': 'Pruebitax2 Dia',
+    'location': '800 Howard St., San Francisco, CA 94103',
+    'description': 'Pues una prueba',
+    'start': {
+        'dateTime': '2022-05-10T09:00:00',  
+        'timeZone': 'Europe/Madrid',
+    },
+    'end': {
+        'dateTime': '2022-05-10T10:00:00',
+        'timeZone': 'Europe/Madrid',
+    },
+    
+    
+    'reminders': {
+        'useDefault': False,
+        'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+        ],
+    },
+    }
+
+
+    """'attendees': [
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+    ],"""
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
+
+
+
+    print("created event")
+    print("id: ", event['id'])
+    print("summary: ", event['summary'])
+    print("starts at: ", event['start']['dateTime'])
+    print("ends at: ", event['end']['dateTime'])
+
+if __name__ == '__main__':
+   main()
+
+def eliminarEventoID(eventID):
+    service = get_calendar_service()
+    service.events().delete(calendarId='primary', eventId= eventID ).execute()
+
+def eliminarEventoTitle(title):
+    service = get_calendar_service()
+    events = service.events().list(calendarId='primary', singleEvents=True, q="Pruebitax2 Dia").execute()
+    for event in events['items']:
+        eliminarEventoID(event['id'])
+    if events['items'] == []:
+        print("No hay eventos con este titulo")
+
+
+def buscarEventoTitle(title):
+    service = get_calendar_service()
+    events = service.events().list(calendarId='primary', singleEvents=True, q=title).execute()
+    for event in events['items']:
+        print( event['summary'])
+        print( event['id'])
+        print( event['location'])
+        print(event['start']['dateTime'])
+        print(event['end']['dateTime'])
+    if events['items'] == []:
+        print("No hay eventos con este titulo")
+    return events
+
+
+def crearEvento(title, description, latitude, longitude, startDateTime, endDateTime):
+
+    service = get_calendar_service()
+    location = obtenerLocation(latitude, longitude)
+    event = {
+    'summary': title,
+    'location': str(location),
+    'description': description,
+    'start': {
+        'dateTime': startDateTime,  
+        'timeZone': 'Europe/Madrid',
+    },
+    'end': {
+        'dateTime': endDateTime,
+        'timeZone': 'Europe/Madrid',
+    },
+    
+    
+    'reminders': {
+        'useDefault': False,
+        'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+        ],
+    },
+    }
+
+
+    """'attendees': [
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+    ],"""
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
+
+
+
+    print("created event")
+    print("id: ", event['id'])
+    print("summary: ", event['summary'])
+    print("starts at: ", event['start']['dateTime'])
+    print("ends at: ", event['end']['dateTime'])
+
+
+def obtenerLocation(latitude, longitude):
+    # calling the nominatim tool
+    geoLoc = Nominatim(user_agent="GetLoc")
+    # passing the coordinates
+    locname = geoLoc.reverse(str(latitude)+","+ str(longitude))
+    print (locname)
+    return locname
+
+def editarEventoTitle(oldTitle, newTitle):
+    service = get_calendar_service()
+    events = buscarEventoTitle(oldTitle)
+    for event in events['items']:
+        #modificar aqui cada evento
+        event['summary'] = newTitle
+        updated_event = service.events().update(calendarId='primary', eventId=event['id'], body=event).execute()
+        print( updated_event['updated'])
+
+def editarEventoDate(title, dateTimeStart, dateTimeEnd):
+    service = get_calendar_service()
+    events = buscarEventoTitle(title)
+    for event in events['items']:
+        #modificar aqui cada evento
+        event['start']['dateTime'] = dateTimeStart
+        event['end']['dateTime'] = dateTimeEnd
+        updated_event = service.events().update(calendarId='primary', eventId=event['id'], body=event).execute()
+        print( updated_event['updated'])
+
+def editarEventoDesciption(title, description):
+    service = get_calendar_service()
+    events = buscarEventoTitle(title)
+    for event in events['items']:
+        #modificar aqui cada evento
+        event['description'] = description
+        updated_event = service.events().update(calendarId='primary', eventId=event['id'], body=event).execute()
+        print( updated_event['updated'])
