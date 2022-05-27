4a3c484fe0e3d6465df47cb5d4e391667628f3a7
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-05-27 18:16:43 +0200

    task #223: Implement unban endpoint

--- app/module_admin/controllers.py
+++ app/module_admin/controllers.py
@@ -8,8 +8,9 @@ from app import db, hashing
 
 # Import module models
 from app.module_admin.models import Admin
-from app.module_users.models import User, SocialOutAuth
+from app.module_users.models import BannedEmails, User, SocialOutAuth
 from app.module_users.utils import generate_tokens
+from app.utils.email import send_email
 
 # Define the blueprint: 'admin', set its url prefix: app.url/admin
 module_admin_v1 = Blueprint('admin', __name__, url_prefix='/v1/admin')
@@ -39,3 +40,33 @@ def login():
     if not hashing.check_value(socialout_auth.pw, password, salt=socialout_auth.salt):
         return jsonify({'error_message': 'Email or password are wrong.'}), 400 
     return generate_tokens(str(user.id)), 200
+
+
+@module_admin_v1.route('/unban', methods=['POST'])
+@jwt_required(optional=False)
+def unban():
+    auth_id = get_jwt_identity()
+    if not Admin.exists(auth_id):
+        return jsonify({'error_message': 'Only administrators can make this action.'}), 403
+    if not (request.json and 'email' in request.json):
+        return jsonify({'error_message': 'Missing email in json body.'}), 400
+    
+    email = request.json['email']
+    reason = request.json['reason']
+
+    ban = BannedEmails.query.filer_by(email = email).first()
+    if ban == None:
+        return jsonify({'error_message': 'This email is not in the banned emails list.'}), 404
+
+    ban_reason = ban.reason
+    ban.delete()
+
+    email_body = 'Hey there! Welcome back to SocialOut.\n\nYou have been unbaned by one of our staff members, \
+        please behave properly this time and do not repeat your past mistakes.\n\n'
+    if ban_reason != None:
+        email_body += f'We remind you the reason you were banned for: {ban_reason}\n\n'
+    if reason != None:
+        email_body += f'This is the reason for your unban: {reason}\n\n'
+    email_body += 'See you around!\nSocialOut team.'
+    send_email(email, 'Unban from SocialOut notice.', email_body)
+    return jsonify({'success': True}), 200
